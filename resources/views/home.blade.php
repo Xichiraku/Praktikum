fd0382d264ec4d1bea367c8c608fee7a90b3ce76
YULIA NABILA <nabilayulia31@gmail.com>
2025-03-26 00:15:47 +0700

    Yulia Nabila menambahkan P4

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+   <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+    Selamat datang di home {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+    
+</div>
